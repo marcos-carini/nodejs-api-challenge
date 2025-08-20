f2806c5517ddc8df8ca021ba0bdbe2522e66930c
Marcos Vinicius <marcos.carini22@gmail.com>
2025-08-20 02:27:13 -0300

    refactor: start refactoring server.ts endpoints to use drizzle and real database connection

--- server.ts
+++ server.ts
@@ -1,5 +1,7 @@
 import fastify from 'fastify';
 import crypto from 'node:crypto';
+import { db } from './src/database/client.ts';
+import { courses } from './src/database/schema.ts';
 
 const server = fastify({
   logger: {
@@ -13,55 +15,61 @@ const server = fastify({
   },
 });
 
-const courses = [
-  {id: '1', title: 'NodeJS'},
-  {id: '2', title: 'ReactJS'},
-  {id: '3', title: 'React Native'},
-]
+// const courses = [
+//   {id: '1', title: 'NodeJS'},
+//   {id: '2', title: 'ReactJS'},
+//   {id: '3', title: 'React Native'},
+// ]
 
-server.get('/courses', () => {
-  return { courses };
+server.get('/courses', async (request, reply) => {
+
+  const result = await db.select({
+    id: courses.id,
+    title: courses.title,
+  }).from(courses)
+
+  return reply.send({ courses: result });
 })
 
-server.get('/courses/:id', (request, reply) => {
-  type Params = {
-    id: string; 
-  }
+// server.get('/courses/:id', (request, reply) => {
+//   type Params = {
+//     id: string; 
+//   }
 
-  const params = request.params as Params;
-  const courseId = params.id;
+//   const params = request.params as Params;
+//   const courseId = params.id;
 
-  const course = courses.find(course => course.id === courseId);
+//   const course = courses.find(course => course.id === courseId);
 
-  if (course) {
-    return { course };
-  }
+//   if (course) {
+//     return { course };
+//   }
 
-  return reply.status(404).send({ error: 'Course not found' });
-})
+//   return reply.status(404).send({ error: 'Course not found' });
+// })
 
-server.post('/courses', (request, reply) => {
+// server.post('/courses', (request, reply) => {
 
-   type Body = {
-    title: string; 
-  }
+//    type Body = {
+//     title: string; 
+//   }
 
-  const courseId = crypto.randomUUID();
+//   const courseId = crypto.randomUUID();
 
-  const body = request.body as Body;
-  const courseTitle = body.title;
+//   const body = request.body as Body;
+//   const courseTitle = body.title;
 
-  if (!courseTitle) {
-    return reply.status(400).send({ message: 'Título é obrigatório' });
-  }
+//   if (!courseTitle) {
+//     return reply.status(400).send({ message: 'Título é obrigatório' });
+//   }
 
-  courses.push({
-    id: courseId,
-    title: courseTitle,
-  })
+//   courses.push({
+//     id: courseId,
+//     title: courseTitle,
+//   })
 
-  return reply.status(201).send({ courseId });
-})
+//   return reply.status(201).send({ courseId });
+// })
 
 server.listen({port: 3333}).then(() => {
   console.log('Server running on http://localhost:3333')
