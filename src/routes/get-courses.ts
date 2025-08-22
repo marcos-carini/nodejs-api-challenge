f6fd5c251dff3be416ffb2546b3ab4c13a565222
Marcos Vinicius <marcos.carini22@gmail.com>
2025-08-22 20:53:12 -0300

    feat: add sorting and pagination to get all routes

--- src/routes/get-courses.ts
+++ src/routes/get-courses.ts
@@ -2,12 +2,18 @@ import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { db } from '../database/client.ts';
 import { courses } from '../database/schema.ts';
 import z from 'zod';
+import { ilike, asc, SQL, and } from 'drizzle-orm';
 
 export const getCoursesRoute: FastifyPluginAsyncZod = async (server) =>{
   server.get('/courses', {
     schema: {
       tags: ['courses'],
       summary: 'Get all courses',
+      querystring: z.object({
+        search: z.string().optional(),
+        orderBy: z.enum(['id', 'title']).optional().default('id'),
+        page: z.coerce.number().optional().default(1),
+      }),
       response: {
         200: z.object({
           courses: z.array(
@@ -15,17 +21,34 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) =>{
               id: z.uuid(),
               title: z.string(),
             })
-          )
+          ),
+          total: z.number(),
         })
       }
     }
   } , async (request, reply) => {
+    const { search, orderBy, page } = request.query;
+
+    const conditions: SQL[] = [];
+
+    if (search) {
+      conditions.push(ilike(courses.title, `%${search}%`));
+    }
+
+    const [result, total] = await Promise.all([
+      db
+        .select({
+          id: courses.id,
+          title: courses.title,
+        })
+        .from(courses)
+        .orderBy(asc(courses[orderBy]))
+        .offset((page - 1) * 2)
+        .limit(2)
+        .where(and(...conditions)),
+      db.$count(courses, and(...conditions))
+    ])
   
-    const result = await db.select({
-      id: courses.id,
-      title: courses.title,
-    }).from(courses)
-  
-    return reply.send({ courses: result });
+    return reply.send({ courses: result, total });
   })
 }
