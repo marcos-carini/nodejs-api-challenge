a23db545b8eae5b0e16b1ccade69ffdc8367b790
Marcos Vinicius <marcos.carini22@gmail.com>
2025-08-26 05:21:29 -0300

    chore: add test coverage reporting

--- src/routes/get-course-by-id.test.ts
+++ src/routes/get-course-by-id.test.ts
@@ -20,3 +20,13 @@ test('get a course by id', async () => {
     }
   })
 })
+
+test('return 404 for non existing courses', async () => {
+  await server.ready()
+
+  const response = request(server.server)
+  .get(`/courses/e7c56fcf-d023-408e-b4ad-1fab8e256a94`)
+
+  expect((await response).status).toEqual(404)
+  
+})
